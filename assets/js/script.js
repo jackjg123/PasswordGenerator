d241deec3017c6320d0cea641df56789370855ee
Jack <131559096+jackjg123@users.noreply.github.com>
2023-06-15 23:55:52 -0400

    I pulled starter html from my instructors. I added some css styling. Also I added the getPasswordCriteria function, created promps for the user criteria, and gave values to the created passwordCriteria variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,99 +1,37 @@
+// selects the button element
 var generateBtn = document.querySelector("#generate");
-
-function getPasswordOptions() {
-
+// Guides the user through the password criteria.
+function getPasswordCriteria() {
+  // use method parseInt() with a value of 10 using the radix parameter of ten
+  // radix of 10 doesn't return letters or whitespace.
   var length = parseInt(
-    prompt("How many characters would you like your password to be?"),
-    10
+    prompt("How many characters do you want to generate?"), 10
   );
-    if (length < 8 || length > 128) {
-      alert('Password length must be between 8 and 128 characters');
-            return null;
-          }
+  // checks to make sure the length value is a number between 8 and 128.
+  // Returns null if length is below 8 or above 128.
+  if (length < 8 || length > 128) {
+    alert("Please enter a number between 8 and 128");
+    return null;
+  }
+  // prompts the user to enter a only numbers and returns null if length is not a number.
+  // sets the constructor parameter
   if (Number.isNaN(length)) {
-    alert('Password length must be provided as a number');
-  return null;
+    alert("The length of your password must be a number.");
+    return null;
   }
-  var hasSpecialCharacters = confirm(
-      "Click ok to include special characters"
-    );
-  var hasUppercaseCharacters = confirm(
-    "Click ok to include uppercase characters"
-  );
-  var hasLowercaseCharacters = confirm(
-      "Click ok to include lowercase characters"
-    );
-   var hasNumbers = confirm(
-    "Click ok to include numbers"
-   ) 
-    var passwordOptions = {
-      length: length, 
-      hasSpecialCharacters: hasSpecialCharacters,
-      hasUppercaseCharacters: hasUppercaseCharacters,
-      hasLowercaseCharacters: hasLowercaseCharacters,
-      hasNumbers: hasNumbers,
-    };
+  // sets the value of the variable to get the user's chosen password criteria.
+  var specialCharacters = confirm("Click ok to include special characters");
+  var upperCaseCharacters = confirm("Click ok to include upper case characters");
+  var lowerCaseCharacters = confirm("Click ok to include lower case characters");
+  var hasNumbers = confirm("click ok to include numbers");
 
-    return passwordOptions;
+  var passwordCriteria = {
+    length: length,
+    specialCharacters: specialCharacters,
+    upperCaseCharacters: upperCaseCharacters,
+    lowerCaseCharacters: lowerCaseCharacters,
+    hasNumbers: hasNumbers,
   }
+  return passwordCriteria;
+}
 
-  function getRandom(arr) {
-    var randIndex = Math.floor(Math.random() * arr.length);
-    var randElement = arr[randIndex];
-  
-    return randElement;
-  }
-
-  function generatePassword() {
-
-    var options = getPasswordOptions();
-    var result = [];
-    var possibleCharacters = [];
-    var guaranteedCharacters = [];
-
-    if (!options) return null;
-
-    var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-    var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var numberCharacters = "0123456789";
-    var specialCharacters = "!@#$%^&*()_+-=[]{}|:;\"<>,.?/~"
-
-    if (options.hasSpecialCharacters) {
-      possibleCharacters = possibleCharacters.concat(specialCharacters);
-      guaranteedCharacters.push(getRandom(specialCharacters));
-    }
-    if (options.hasUppercaseCharacters) {
-      possibleCharacters = possibleCharacters.concat(uppercaseCharacters);
-      guaranteedCharacters.push(getRandom(uppercaseCharacters));
-    }
-    if (options.hasLowercaseCharacters) {
-      possibleCharacters = possibleCharacters.concat(lowercaseCharacters);
-      guaranteedCharacters.push(getRandom(lowercaseCharacters));
-    }
-    if (options.hasNumbers) {
-      possibleCharacters = possibleCharacters.concat(numberCharacters);
-      guaranteedCharacters.push(getRandom(numberCharacters)); 
-    }
-
-    for (var i = 0; i < options.length - guaranteedCharacters.length; i++) {
-      var possibleCharacter = getRandom(possibleCharacters);
-      result.push(possibleCharacter);
-    }
-  
-    result = result.concat(guaranteedCharacters.split("")).sort(function () {
-      return 0.5 - Math.random();
-    });
-    
-      return result.join('');
-      
-  }
-  
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  }
-    generateBtn.addEventListener("click", writePassword);
-
-  
