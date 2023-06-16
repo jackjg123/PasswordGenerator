f7d1ce693ab23b1c94e3144eda8c35fe1c1d042b
Jack <131559096+jackjg123@users.noreply.github.com>
2023-06-16 13:18:31 -0400

    Added the randomCharacters function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ function getPasswordCriteria() {
   // use method parseInt() with a value of 10 using the radix parameter of ten
   // radix of 10 doesn't return letters or whitespace.
   var length = parseInt(
-    prompt("How many characters do you want to generate?"), 10
+    prompt("How many characters do you would you like to generate?"), 10
   );
   // checks to make sure the length value is a number between 8 and 128.
   // Returns null if length is below 8 or above 128.
@@ -23,8 +23,8 @@ function getPasswordCriteria() {
   var specialCharacters = confirm("Click ok to include special characters");
   var upperCaseCharacters = confirm("Click ok to include upper case characters");
   var lowerCaseCharacters = confirm("Click ok to include lower case characters");
-  var hasNumbers = confirm("click ok to include numbers");
-
+  var hasNumbers = confirm("Click ok to include numbers");
+  // set the variables to be the properties of the passwordCriteria.
   var passwordCriteria = {
     length: length,
     specialCharacters: specialCharacters,
@@ -34,4 +34,14 @@ function getPasswordCriteria() {
   }
   return passwordCriteria;
 }
+
+function randomCharacters(arr) {
+  //Math.random() function calculates a random number between 0 and 1, and it is multiplied by the number given by the array length. The Math.floor() function then rounds that value down to the nearest integer.
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  // allows the selection of a random element from an array based one the randomly generated index value
+  var randomElement = arr[randomIndex];
+  
+  return randomElement;
+}
+
 
