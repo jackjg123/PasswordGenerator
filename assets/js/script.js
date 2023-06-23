3535bead18aca5556291e6aad839703744f792b8
Jack <131559096+jackjg123@users.noreply.github.com>
2023-06-23 01:50:52 -0400

    added comments to css html and javascript. also wrote readme and changed some css styles.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
-// selects the button element
 var generateBtn = document.querySelector('#generate');
 
 console.log(generateBtn);
 
-// Guides the user through the password criteria.
+// prompts the user for password Criteria
 function getPasswordCriteria() {
-  // use method parseInt() with a value of 10 using the radix parameter of ten
-  // radix of 10 doesn't return letters or whitespace.
   var length = parseInt(
     prompt('How many characters do you would you like to generate?'),
 
@@ -15,28 +12,23 @@ function getPasswordCriteria() {
 
   console.log(length);
 
-  // checks to make sure the length value is a number between 8 and 128.
-  // Returns null if length is below 8 or above 128.
+  // Validates the password length chosen by user
   if (length < 8 || length > 128) {
     alert('Please enter a number between 8 and 128');
 
     console.log('Please enter a number between 8 and 128');
 
-    // reloads page after prompt
     return null;
   }
-  // prompts the user to enter a only numbers and returns null if length is not a number.
-  // sets the constructor parameter
+
   if (Number.isNaN(length)) {
     alert('The length of your password must be a number.');
 
     console.log('The length of your password must be a number.');
 
-    // reloads page after prompt
     return null;
   }
 
-  // sets the value of the variable to get the user's chosen password criteria.
   var hasSpecialCharacters = confirm('Click ok to include special characters');
 
   console.log('User selected to include special characters');
@@ -56,7 +48,7 @@ function getPasswordCriteria() {
 
   console.log('User selected to include numbers');
 
-  // set the variables to be the properties of the passwordCriteria.
+  //Create a new object setting the password criteria.
   var passwordCriteria = {
     length: length,
 
@@ -68,32 +60,33 @@ function getPasswordCriteria() {
 
     hasNumbers: hasNumbers,
   };
-  console.log(passwordCriteria, 'Length: 15');
+  console.log(passwordCriteria.length);
 
   return passwordCriteria;
 }
 
+//generates random character
 function randomCharacter(arr) {
-  //Math.random() function calculates a random number between 0 and 1, and it is multiplied by the number given by the array length. The Math.floor() function then rounds that value down to the nearest integer.
   var randomIndex = Math.floor(Math.random() * arr.length);
-  // allows the selection of a random element from an array based one the randomly generated index value
+
   var randomElement = arr[randomIndex];
 
   return randomElement;
 }
 
+//declare objects to empty arrays
 function generatePassword() {
-  //assigns the users returned value of the password criteria to options variable
   var options = getPasswordCriteria();
-  //declares arrays and stores the values for possible, guaranteed, and the results.
+
   var results = [];
 
   var possibleCharacter = [];
-  // guarantees one value for each option chosen by the user
+
   var guaranteedCharacters = [];
 
   if (!options) return null;
-  //all possible characters are set in a string. note the \\ allows a single backslash in the string
+
+  //set the string with all the characters corresponding to the each passwordCriteria
   var specialCharacters = '"!@#$%^&*()_+-=[]{}|:;\\"<>,.?/~'.split('');
 
   console.log(specialCharacters);
@@ -110,19 +103,19 @@ function generatePassword() {
 
   console.log(numberCharacters);
 
-  // Checks each option to see which were selected.
+  // set a guaranteed character for each selected Criteria
+  // include special characters
   if (options.hasSpecialCharacters) {
-    //combines possibleCharacters array with specialCharacters string.
     possibleCharacter = possibleCharacter.concat(specialCharacters);
 
     console.log(possibleCharacter);
-    //inside the push method the the specialCharacters array runs through the randomCharacters function.
-    //the push method pushes the result to the guaranteedCharacters array.
+
     guaranteedCharacters.push(randomCharacter(specialCharacters));
 
     console.log(guaranteedCharacters);
   }
 
+  // include uppercase
   if (options.hasUpperCaseCharacters) {
     possibleCharacter = possibleCharacter.concat(upperCaseCharacters);
 
@@ -133,6 +126,7 @@ function generatePassword() {
     console.log(guaranteedCharacters);
   }
 
+  //include lowercase
   if (options.hasLowerCaseCharacters) {
     possibleCharacter = possibleCharacter.concat(lowerCaseCharacters);
 
@@ -143,6 +137,7 @@ function generatePassword() {
     console.log(guaranteedCharacters);
   }
 
+  //include numbers
   if (options.hasNumbers) {
     possibleCharacter = possibleCharacter.concat(numberCharacters);
 
@@ -152,24 +147,37 @@ function generatePassword() {
 
     console.log(guaranteedCharacters);
   }
-  //
 
+  // get random possible characters until password length is met. (after guaranteed characters are set)
   while (results.length < options.length - guaranteedCharacters.length) {
     var randomChar = randomCharacter(possibleCharacter);
-    if (!results.includes(randomChar)) {
+    console.log(
+      '🚀 ~ file: script.js:143 ~ generatePassword ~ randomChar:',
+      randomChar
+    );
+    if (!results.includes(possibleCharacter)) {
       results.push(randomChar);
     }
   }
 
+  // combines the random results with with guaranteed characters.
   var passwordArray = guaranteedCharacters.concat(results);
   var password = passwordArray.join('');
   return password;
 }
+
+// checks if password meets criteria. Displays the password.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
   passwordText.value = password;
+  console.log(
+    '🚀 ~ file: script.js:156 ~ writePassword ~ passwordText.value:',
+    passwordText.value
+  );
   if (passwordText.value !== password) console.log('Fail');
   else console.log('Here is your new password!');
 }
+
+// clicking button initiates the code.
 generateBtn.addEventListener('click', writePassword);
